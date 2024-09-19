81ab7fa0a4cbdcef8bd3d2deca911fd80c359b0d
Yigid BALABAN <fyb@fybx.dev>
2024-09-19 21:49:17 +0300

    refactor into TagGallery

---
+++ src/lib/components/TagGallery.svelte
@@ -0,0 +1,59 @@
+<script>
+	export let heatmap = false;
+	export let data;
+	let uniqueCategories;
+	let maxCount;
+
+	console.log(data);
+
+	if (heatmap) {
+		uniqueCategories = data.uniqueCategories;
+		maxCount = Math.max(...uniqueCategories.map((category) => category.count));
+	}
+
+	function getTagColor(count) {
+		const maxColor = 100;
+		const minColor = 0;
+		const percentage = (count / maxCount) * (maxColor - minColor) + minColor;
+		return `rgba(34, 151, 153, ${percentage}%)`;
+	}
+</script>
+
+<div class="tag-grid">
+	{#if heatmap}
+		{#each uniqueCategories as tag}
+			<a class="tag" href="/tags/{tag.title}" style="--tag-color: {getTagColor(tag.count)};">
+				{tag.title} ({tag.count})
+			</a>
+		{/each}
+	{:else}
+		{#each data as tag}
+			<a class="tag" href="/tags/{tag}">{tag}</a>
+		{/each}
+	{/if}
+</div>
+
+<style lang="scss">
+	.tag-grid {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+	}
+
+	.tag {
+		background-color: var(--tag-color, rgba(34, 151, 153, 100%));
+		color: #fff;
+		text-align: center;
+		text-decoration: none !important;
+		padding: 0.5rem 0.75rem;
+		border-radius: 0.5rem;
+		font-weight: bold;
+		box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+		transition: transform 0.3s ease-out;
+
+		&:hover {
+			color: #fff !important;
+			transform: scale(1.05);
+		}
+	}
+</style>
--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -1,15 +1,7 @@
 <script>
+	import TagGallery from '$lib/components/TagGallery.svelte';
+
 	export let data;
-	const { uniqueCategories } = data;
-
-	const maxCount = Math.max(...uniqueCategories.map((category) => category.count));
-
-	function getTagColor(count) {
-		const maxColor = 100;
-		const minColor = 0;
-		const percentage = (count / maxCount) * (maxColor - minColor) + minColor;
-		return `rgba(34, 151, 153, ${percentage}%)`;
-	}
 </script>
 
 <svelte:head>
@@ -19,37 +11,5 @@
 <div class="compressed-content">
 	<h1 class="h2">All tags</h1>
 	<p>Check out what we've talked about so far</p>
-
-	<div class="tag-grid">
-		{#each uniqueCategories as tag}
-			<a class="tag" href="/tags/{tag.title}" style="--tag-color: {getTagColor(tag.count)};">
-				{tag.title} ({tag.count})
-			</a>
-		{/each}
-	</div>
+	<TagGallery heatmap={true} {data} />
 </div>
-
-<style lang="scss">
-	.tag-grid {
-		display: flex;
-		flex-wrap: wrap;
-		gap: 1rem;
-	}
-
-	.tag {
-		background-color: var(--tag-color, hsl(120, 60%, 70%));
-		color: #fff;
-		text-align: center;
-		text-decoration: none !important;
-		padding: 1rem;
-		border-radius: 0.5rem;
-		font-weight: bold;
-		box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-		transition: transform 0.3s ease-out;
-
-		&:hover {
-			color: #fff !important;
-			transform: scale(1.05);
-		}
-	}
-</style>
