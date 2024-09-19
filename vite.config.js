56344df073ee625a9322ad33bd2cfd5516676e10
Yigid BALABAN <fyb@fybx.dev>
2024-09-19 20:42:05 +0300

    can't use modern-compiler because it hangs after build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler'
+        silenceDeprecations: ["legacy-js-api"],
       },
     },
   },
